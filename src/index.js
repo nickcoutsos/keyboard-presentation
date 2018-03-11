bf8ca94d53fbf2d7a2e54652cc02654b695da64e
Nick Coutsos <nick@coutsos.com>
2018-03-11 14:08:26 -0400

    Tween between keyboard layouts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { DirectionalLight, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
+import { AmbientLight, DirectionalLight, PerspectiveCamera, Quaternion, Scene, Vector3, WebGLRenderer } from 'three';
 import * as layouts from './layouts'
 import * as dactyl from './dactyl'
 import './style.css'
@@ -17,10 +17,41 @@ function resize () {
   renderer.render(scene, camera)
 }
 
-// function animate() {
-//   requestAnimationFrame(animate)
-//   renderer.render(scene, camera)
-// }
+const keyboard = layouts.makeKeyboard(layouts.ergodox)
+const source = layouts.makeKeyboard(layouts.ergodox)
+const target = dactyl.makeKeyboard()
+
+const sourceKeyboardMap = layouts.makeKeymap(source)
+const targetKeyboardMap = layouts.makeKeymap(target)
+// const tweenKeyboardMap = layouts.makeKeymap(keyboard)
+
+function animate(t=0) {
+  const t_ = (1 + Math.sin(t / 1000)) / 2
+
+  keyboard.traverse(node => {
+    const { quaternion, position } = node
+    const { id } = node.userData
+    if (id) {
+      const a = sourceKeyboardMap[id]
+      const b = targetKeyboardMap[id]
+
+      if (!(a && b)) {
+        return
+      }
+
+      const aQuat = a.getWorldQuaternion()
+      const bQuat = b.getWorldQuaternion()
+      const aPos =  a.getWorldPosition()
+      const bPos =  b.getWorldPosition()
+
+      Quaternion.slerp(aQuat, bQuat, quaternion, t_)
+      position.copy(aPos.clone().lerp(bPos, t_))
+    }
+  })
+
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
 
 const light = new DirectionalLight('white', 1)
 light.position.set(0, 5, 5)
@@ -29,13 +60,15 @@ camera.position.set(-5, -10, 10)
 camera.up.set(0, 0, 1)
 camera.lookAt(new Vector3(0, 0, 0))
 
-scene.add(camera, light)
-// scene.add(layouts.makeKeyboard(layouts.apple))
-scene.add(dactyl.makeKeyboard())
+scene.add(camera, light, new AmbientLight('powderblue', .8))
+scene.add(layouts.makeKeyboard(layouts.ergodox))
+// scene.add(dactyl.makeKeyboard())
+scene.add(keyboard)
+
 
 renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1)
 document.querySelector('#app').appendChild(renderer.domElement)
 window.addEventListener('resize', resize)
 
 resize()
-// animate()
+animate()
