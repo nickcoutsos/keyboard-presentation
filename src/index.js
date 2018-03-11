35619d93fa8df9e8db7e6440d634b79b1cf4aaa5
Nick Coutsos <nick@coutsos.com>
2018-03-11 17:31:03 -0400

    refactor keyboard tweening

--- src/index.js
+++ src/index.js
@@ -1,74 +1,49 @@
 import { AmbientLight, DirectionalLight, PerspectiveCamera, Quaternion, Scene, Vector3, WebGLRenderer } from 'three';
 import * as layouts from './layouts'
 import * as dactyl from './dactyl'
+import tween from './tween'
 import './style.css'
 
+const container = document.querySelector('#app')
 const renderer = new WebGLRenderer({ antialias: true, alpha: true });
 const camera = new PerspectiveCamera(75, 1, 0.1, 1000)
 const scene = new Scene()
 
+renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1)
+container.appendChild(renderer.domElement)
+window.addEventListener('resize', resize)
+
+function renderFrame () {
+  renderer.render(scene, camera)
+}
+
 function resize () {
-  const element = document.querySelector('#app')
-  const { width, height } = element.getBoundingClientRect()
+  const { width, height } = container.getBoundingClientRect()
 
   renderer.setSize(width, height)
   camera.aspect = width / height
   camera.updateProjectionMatrix()
-  renderer.render(scene, camera)
+  renderFrame()
 }
 
-const keyboard = layouts.makeKeyboard(layouts.ergodox)
-const source = layouts.makeKeyboard(layouts.ergodox)
-const target = dactyl.makeKeyboard()
-
-const sourceKeyboardMap = layouts.makeKeymap(source)
-const targetKeyboardMap = layouts.makeKeymap(target)
-// const tweenKeyboardMap = layouts.makeKeymap(keyboard)
-
-function animate(t=0) {
-  const t_ = (1 + Math.sin(t / 1000)) / 2
-
-  keyboard.traverse(node => {
-    const { quaternion, position } = node
-    const { id } = node.userData
-    if (id) {
-      const a = sourceKeyboardMap[id]
-      const b = targetKeyboardMap[id]
-
-      if (!(a && b)) {
-        return
-      }
-
-      const aQuat = a.getWorldQuaternion()
-      const bQuat = b.getWorldQuaternion()
-      const aPos =  a.getWorldPosition()
-      const bPos =  b.getWorldPosition()
-
-      Quaternion.slerp(aQuat, bQuat, quaternion, t_)
-      position.copy(aPos.clone().lerp(bPos, t_))
-    }
-  })
-
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
-}
-
-const light = new DirectionalLight('white', 1)
-light.position.set(0, 5, 5)
+const ambientLight = new AmbientLight('powderblue', .8)
+const directionalLight = new DirectionalLight('white', 1)
+directionalLight.position.set(0, 5, 5)
 
 camera.position.set(-5, -10, 10)
 camera.up.set(0, 0, 1)
 camera.lookAt(new Vector3(0, 0, 0))
 
-scene.add(camera, light, new AmbientLight('powderblue', .8))
-scene.add(layouts.makeKeyboard(layouts.ergodox))
-// scene.add(dactyl.makeKeyboard())
-scene.add(keyboard)
+const source = layouts.makeKeyboard(layouts.split)
+const target = layouts.makeKeyboard(layouts.ergodox)
+const animate = tween(renderFrame, source, target, 2000)
 
-
-renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1)
-document.querySelector('#app').appendChild(renderer.domElement)
-window.addEventListener('resize', resize)
+scene.add(
+  camera,
+  directionalLight,
+  ambientLight,
+  source
+)
 
 resize()
 animate()
---
+++ src/tween.js
@@ -0,0 +1,34 @@
+import { Quaternion } from 'three'
+import * as layouts from './layouts'
+import * as animation from './animation'
+
+export default (renderFrame, source, target, duration) => {
+  const original = source.clone()
+  const sourceKeyboardMap = layouts.makeKeymap(original)
+  const targetKeyboardMap = layouts.makeKeymap(target)
+
+  return animation.animate(t => {
+    source.traverse(node => {
+      const { quaternion, position } = node
+      const { id } = node.userData
+      if (id) {
+        const a = sourceKeyboardMap[id]
+        const b = targetKeyboardMap[id]
+
+        if (!(a && b)) {
+          return
+        }
+
+        const aQuat = a.getWorldQuaternion()
+        const bQuat = b.getWorldQuaternion()
+        const aPos =  a.getWorldPosition()
+        const bPos =  b.getWorldPosition()
+
+        Quaternion.slerp(aQuat, bQuat, quaternion, t)
+        position.copy(aPos.clone().lerp(bPos, t))
+      }
+    })
+
+    renderFrame()
+  }, duration)
+}
