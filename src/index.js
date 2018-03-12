5562e44b3726bc0420878ffbc96e3c920db3f448
Nick Coutsos <nick@coutsos.com>
2018-03-11 23:56:03 -0400

    Tween through all layouts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { AmbientLight, DirectionalLight, PerspectiveCamera, Quaternion, Scene, Vector3, WebGLRenderer } from 'three';
+import { AmbientLight, DirectionalLight, Object3D, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
 import * as layouts from './layouts'
 import * as dactyl from './dactyl'
 import tween from './tween'
@@ -34,16 +34,42 @@ camera.position.set(-5, -10, 10)
 camera.up.set(0, 0, 1)
 camera.lookAt(new Vector3(0, 0, 0))
 
-const source = layouts.makeKeyboard(layouts.split)
-const target = layouts.makeKeyboard(layouts.ergodox)
-const animate = tween(renderFrame, source, target, 2000)
+const wrapper = new Object3D()
+
+const phases = [
+  layouts.makeKeyboard(layouts.classic),
+  layouts.makeKeyboard(layouts.apple),
+  layouts.makeKeyboard(layouts.preonic),
+  layouts.makeKeyboard(layouts.split),
+  layouts.makeKeyboard(layouts.ergodox),
+  dactyl.makeKeyboard()
+]
+
+const tweens = phases.reduce((acc, phase, i, phases) => {
+  if (i === phases.length - 1) {
+    return acc
+  }
+
+  const start = phase.clone()
+  const end = phases[i + 1].clone()
+  const animation = tween(renderFrame, start, end, 2000)
+  return acc.concat(callback => {
+    wrapper.remove(...wrapper.children)
+    wrapper.add(start)
+    wrapper.updateMatrix()
+    renderFrame()
+    setTimeout(() => animation(callback), 1000)
+  })
+}, [])
+
+const tweenAll = tweens.reduce((a, b) => c => a(() => b(c)))
 
 scene.add(
   camera,
   directionalLight,
   ambientLight,
-  source
+  wrapper
 )
 
 resize()
-animate()
+tweenAll()
