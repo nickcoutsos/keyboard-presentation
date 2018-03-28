144a3531931b46e770e31dc38c536515ff9d7d55
Nick Coutsos <nick@coutsos.com>
2018-03-28 00:33:15 -0400

    Tween keyboards by layout named in slide data attribute

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { values } from 'lodash'
-import { Color } from 'three'
+import { Color, Object3D } from 'three'
 import * as animation from './animation'
-import * as layouts from './layouts'
-import * as dactyl from './dactyl'
+import { makeKeymap } from './layouts'
+import * as keyboards from './keyboards'
 import * as materials from './materials'
 import Ruler from './ruler'
 import tween from './tween'
@@ -10,18 +10,10 @@ import * as viewer from './viewer'
 import * as slideshow from './slideshow'
 import './style.scss'
 
-const keyboards = [
-  layouts.makeKeyboard(layouts.classic),
-  layouts.makeKeyboard(layouts.apple),
-  layouts.makeKeyboard(layouts.split),
-  layouts.makeKeyboard(layouts.ergodox),
-  dactyl.makeKeyboard()
-]
-
-const wrapper = keyboards[0].clone()
+const wrapper = new Object3D()
 
 let cancel
-let activeKeymap = layouts.makeKeymap(wrapper)
+let activeKeymap = makeKeymap(wrapper)
 
 const keyHighlight = (event) => {
   const { type, key, repeat, altKey, ctrlKey, metaKey, shiftKey } = event
@@ -69,24 +61,45 @@ const ruler = new Ruler(viewer)
 viewer.resize()
 
 slideshow.initialize()
-slideshow.events.on('slidechanged', ({ previousSlideIndex, previousSlide, slide, state }) => {
-  const prev = keyboards[previousSlideIndex]
-  const next = keyboards[state.slide]
-  const { start, stop } = tween(viewer.renderFrame, wrapper, prev, next, 500)
+slideshow.events.on('slidechanged', ({ previousSlide, slide }) => {
+  const prev = keyboards[previousSlide.dataset.keyboardLayout]
+  const next = keyboards[slide.dataset.keyboardLayout]
 
+  tweenColours(
+    window.getComputedStyle(previousSlide).backgroundColor,
+    window.getComputedStyle(slide).backgroundColor
+  )
+
+  if (prev && next) {
+    tweenKeyboards(prev, next)
+  } else {
+    wrapper.remove(...wrapper.children)
+    if (next) {
+      wrapper.add(...next.children.map(child => child.clone()))
+      viewer.renderFrame()
+    }
+  }
+
+  activeKeymap = makeKeymap(wrapper)
+})
+
+const tweenKeyboards = (begin, end) => {
+  const { start, stop } = tween(viewer.renderFrame, wrapper, begin, end, 500)
   cancel && cancel()
   cancel = stop
 
-  activeKeymap = layouts.makeKeymap(wrapper)
+  activeKeymap = makeKeymap(wrapper)
   start(() => { cancel = null })
+}
 
-  const prevColor = new Color(window.getComputedStyle(previousSlide).backgroundColor)
-  const nextColor = new Color(window.getComputedStyle(slide).backgroundColor)
-  const colorTween = animation.animate(t => {
-    const color = prevColor.clone().lerp(nextColor, t)
+const tweenColours = (begin, end) => {
+  const beginColour = new Color(begin)
+  const endColour = new Color(end)
+
+  animation.animate(t => {
+    const color = beginColour.clone().lerp(endColour, t)
     const ambientLight = viewer.scene.getObjectByName('ambient')
     ambientLight.color = color
-  }, 500)
-
-  colorTween.start()
-})
+    viewer.renderFrame()
+  }, 500).start()
+}
---
+++ src/keyboards.js
@@ -0,0 +1,8 @@
+import * as layouts from './layouts'
+import * as dactylLayout from './dactyl'
+
+export const classic = layouts.makeKeyboard(layouts.classic)
+export const apple = layouts.makeKeyboard(layouts.apple)
+export const split = layouts.makeKeyboard(layouts.split)
+export const ergodox = layouts.makeKeyboard(layouts.ergodox)
+export const dactyl = dactylLayout.makeKeyboard()
