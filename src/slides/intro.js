258c41f95b800f79c66666e2e4042edfc9f09679
Nick Coutsos <nick@coutsos.com>
2018-03-31 13:11:55 -0400

    Add sound effects

--- src/slides/intro.js
+++ src/slides/intro.js
@@ -1,6 +1,12 @@
 import * as parts from '../parts'
 import * as viewer from '../viewer'
 
+const recordScratch = new Audio('dist/assets/record-scratch.wav')
+const rewind = new Audio('dist/assets/rewind.wav')
+
+rewind.volume = .4
+rewind.loop = true
+
 export const initialize = () => {
   viewer.scene.add(parts.scene)
   parts.scene.visible = false
@@ -13,6 +19,7 @@ export const activate = (state) => {
 }
 
 export const deactivate = () => {
+  rewind.pause()
   parts.scene.visible = false
   viewer.toggleEffect('pause', false)
   viewer.renderFrame()
@@ -31,6 +38,10 @@ const animate = () => {
 }
 
 export const fragment = (state) => {
+  if (state.previousFragment == 1) {
+    rewind.pause()
+  }
+
   switch (state.fragment) {
     case -1:
       rotation = 0.035
@@ -40,10 +51,14 @@ export const fragment = (state) => {
     case 0:
       rotation = 0
       viewer.toggleEffect('pause', true)
+      if (state.previousFragment < state.fragment) {
+        recordScratch.play()
+      }
       break
     case 1: 
       rotation = -.5
       viewer.toggleEffect('pause', true)
+      rewind.play()
       break
     case 2:
       parts.scene.visible = false
