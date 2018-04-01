af3d2a16d3c9f737db3d3558030b8be01c25035c
Nick Coutsos <nick@coutsos.com>
2018-03-31 23:35:09 -0400

    Add slide actions for classic keyboard

--- src/slides/index.js
+++ src/slides/index.js
@@ -1,4 +1,5 @@
 import * as intro from './intro'
+import * as classic from './classic'
 import * as supports from './supports'
 
-export default { intro, supports }
+export default { intro, classic, supports }
