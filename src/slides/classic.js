162d9f5ca6257ec57374410432a0b5aa7aa8bde3
Nick Coutsos <nick@coutsos.com>
2018-04-06 00:19:35 -0400

    Drop Back to the Future theme, add Leftovers theme and tweak styles

--- src/slides/classic.js
+++ src/slides/classic.js
@@ -2,7 +2,7 @@ const theme = new Audio('dist/assets/back-to-the-future-intro.wav')
 
 export const fragment = (state) => {
   // play theme only when advancing through slideshow
-  if (state.fragment === 0 && state.previousFragment === -1) {
-    theme.play()
-  }
+  // if (state.fragment === 0 && state.previousFragment === -1) {
+  //   theme.play()
+  // }
 }
--- src/slides/outro.js
+++ src/slides/outro.js
@@ -1,8 +1,21 @@
 import { Object3D } from 'three'
+import { clamp } from 'lodash'
 import * as viewer from '../viewer'
 import * as animation from '../animation'
 import { ergodox, dactyl } from '../keyboards'
 import { makeTween } from '../tween'
+
+const music = new Audio('dist/assets/the-leftovers-departure.m4a')
+music.volume = .4
+
+const volumeControl = ({ key }) => {
+  if (key !== 'ArrowUp' && key !== 'ArrowDown') {
+    return
+  }
+
+  const change = .01 * (key === 'ArrowUp' ? 1 : -1)
+  music.volume = clamp(music.volume + change, 0, 1)
+}
 
 const wrapper = new Object3D()
 const tween = makeTween(wrapper, ergodox, dactyl)
@@ -10,7 +23,7 @@ const clip = animation.animate(t => {
   wrapper.rotation.z = t * 2
   tween(Math.pow(t*3, 4))
   viewer.renderFrame()
-}, 50000)
+}, 60000)
 
 export const initialize = () => {
   wrapper.visible = false
@@ -18,11 +31,17 @@ export const initialize = () => {
 }
 
 export const activate = () => {
+  console.log('wtf')
+  window.addEventListener('keydown', volumeControl)
   wrapper.visible = true
   clip.start()
+  music.currentTime = 0
+  music.play()
 }
 
 export const deactivate = () => {
+  window.removeEventListener('keypress', volumeControl)
   wrapper.visible = false
   clip.stop()
+  music.pause()
 }
